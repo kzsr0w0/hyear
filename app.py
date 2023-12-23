f44600f94887e8b0219a9666bc26570d26a01ca7
kzsr0w0 <wkym0508@example.com>
2023-12-23 16:52:19 +0900

    4th commit

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ name = st.text_input("名前を入力してください")
 year = st.number_input("年を入力してください", min_value=2020, max_value=2100, value=2023)
 
 if st.button("あいさつを生成"):
-    response = requests.post("http://127.0.0.1:8000/generate-greeting", json={"name": name, "year": year})
+    #response = requests.post("http://127.0.0.1:8000/generate-greeting", json={"name": name, "year": year})
+    response = requests.post('https://hyear.onrender.com/generate-greeting', json={"name": name, "year": year})
     if response.status_code == 200:
         st.write(response.json()["greeting"])
     else:
