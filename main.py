1ed869fc02589958e11c01d8d48fcee14f76cc5e
kzsr0w0 <wkym0508@example.com>
2023-12-23 18:19:37 +0900

    15th commit

--- main.py
+++ main.py
@@ -2,12 +2,8 @@
 import random
 from fastapi import FastAPI
 from pydantic import BaseModel
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 app = FastAPI()
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = GPT2LMHeadModel.from_pretrained("gpt2")
 
 class GreetingRequest(BaseModel):
     name: str
@@ -17,12 +13,9 @@ def generate_greeting(name, year):
     new_year_words = ["希望", "幸福", "繁栄", "健康", "成功", "平和", "愛", "喜び", "幸運", "夢"]
     random_word = random.choice(new_year_words)
     
-    input_text = f"新年あけましておめでとうございます、{name}さん！{year}年の目標は"
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    output = model.generate(input_ids, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return f"{generated_text} そして、今年も{random_word}が訪れますように"
+    # カスタマイズされたメッセージを生成
+    greeting_message = f"新年あけましておめでとうございます、{name}さん！{year}年も{random_word}が訪れますように"
+    return greeting_message
 
 @app.post("/generate-greeting")
 async def generate_greeting_endpoint(request: GreetingRequest):
